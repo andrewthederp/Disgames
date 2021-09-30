630019ab3c6b337319ab11f6c191951e6167f0c6
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-09-30 13:49:28 +0530

    Create and move stuff into utils.py

--- src/Board.py
+++ src/Board.py
@@ -37,28 +37,3 @@ class Board:
         Returns the seperator of the board"""
 
         return self._default
-
-def edit_board(board, coors, replacment):
-    """takes a list of coors and edits them to whatever replacment is"""
-
-    if not isinstance(board, Board):
-        raise ValueError("board argument can only be a Board object")
-    bord = eval(str(board))
-    bord = Board(len(bord)-1, len(bord[0])-1, board.default, coors, replacment, bord)
-    return bord
-
-def format_board(board):
-    """Formats the nested list to make it look pwetty"""
-    if not isinstance(board, Board):
-        raise ValueError("board argument can only be a Board object")
-    lst = []
-    bord = eval(str(board))
-    for i in bord:
-        scn_lst = []
-        for thing in i:
-            if thing == f"{board.default}":
-                scn_lst.append(f'{board.default} ')
-            else:
-                scn_lst.append(thing)
-        lst.append(''.join(scn_lst))
-    return '\n'.join(lst)
