f45a263854ae2196cf1899e7e5735f140be44da7
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-05 16:22:31 +0530

    More Tests

---
+++ tests/add_command.py
@@ -0,0 +1,6 @@
+from disgames import TicTacToe
+from discord.ext import commands
+
+bot = commands.Bot("!", help_command=None)
+bot.add_command(TicTacToe().command)
+assert len(bot.commands) == 1 # only the tictactoe command
