7aa21dd804598f9b4b8496c259b26c854070e278
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-05 16:07:41 +0530

    Create tests

--- disgames/__init__.py
+++ disgames/__init__.py
@@ -1,14 +1,10 @@
 from .board import Board
 from .ttt import TicTacToe
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from discord.ext import commands
 
 all_games = [TicTacToe]
 
 
-def register_commands(bot: commands.Bot, *, ignore: list = []):
+def register_commands(bot, *, ignore: list = []):
     for cls in all_games:
         if cls in ignore:
             continue
---
+++ tests/register_commands.py
@@ -0,0 +1,6 @@
+from disgames import register_commands
+from discord.ext import commands
+
+bot = commands.Bot("!", help_command=None, case_insensitive=True)
+register_commands(bot)
+assert len(bot.commands) == 1 # only the tictactoe command
