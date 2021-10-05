85c95113bf8f9c094906191087d41878395f45c2
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-10-05 06:49:02 +0200

    Update Board.py

--- src/Board.py
+++ src/Board.py
@@ -1,39 +1,28 @@
-class Board:
-    """A board class that gives a nested which is the board"""
-    def __init__(self, x, y, default='', coordinates = [], replacment = ' ', test=[]):
-        self.x = x
-        self.y = y
-        self._default = default
-        self.coors = coordinates
-        self.replacment = replacment
-        self.test = test
+from .Board import Board
 
-    def __str__(self):
-        lst = []
-        g = [f'{self._default}{i}' for i in range(self.x+1)]
-        lst.append(g)
-        for i in range(self.x):
-            lst.append([f'{self._default}{i+1}'])
-            for _ in range(self.y):
-                lst[i+1].append(f"{self._default}")
-        if self.coors:
-            for x, i in enumerate(self.test):
-                for y, j in enumerate(i):
-                    if self.test[x][y] != f"{self._default} ":
-                        lst[x][y] = self.test[x][y]
-            for x, y in self.coors:
-                lst[int(x)][int(y)] = f"{self._default}{self.replacment}"
-        return str(lst)
+def edit_board(board, coors, replacment):
+    """takes a list of coors and edits them to whatever replacment is"""
 
-    @property
-    def dimensions(self):
-        """Give the dimensions of the board"""
-        return self.x, self.y
+    if not isinstance(board, Board):
+        raise ValueError("board argument can only be a Board object")
+        return
+    bord = eval(str(board))
+    bord = Board(len(bord)-1, len(bord[0])-1, board.seperator, coors, replacment, bord)
+    return bord
 
-    @property
-    def default(self):
-        """TODO: change the name
-
-        Returns the seperator of the board"""
-
-        return self._default
+def format_board(board):
+    """Formats the nested list to make it look pwetty"""
+    if not isinstance(board, Board):
+        raise ValueError("board argument can only be a Board object")
+        return
+    lst = []
+    bord = eval(str(board))
+    for i in bord:
+        scn_lst = []
+        for thing in i:
+            if thing == f"{board.seperator}":
+                scn_lst.append(f'{board.seperator} ')
+            else:
+                scn_lst.append(thing)
+        lst.append(''.join(scn_lst))
+    return '\n'.join(lst)
