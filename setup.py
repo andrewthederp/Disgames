c28fd1741d7323a7af3d87e7e553e4acac86282c
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-05 15:36:48 +0530

    Create setup.py for installation

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="disgames",
+    version="1.0.0a",
+    description="A games module that can be used to instantly add games to your discord bot",
+    long_description=None,
+    author="andrew",
+    maintainer="Marcus",
+    url="https://github.com/andrewthederp/Disgames",
+    license="Apache",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires="discord.py",
+    python_requires='>=3.6',
+    packages=find_packages(include=['disgames', 'disgames.*']),
+)
