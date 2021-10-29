13929e90254e94d0b80c4c5bca93967041fc6082
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-10-29 14:15:28 +0200

    Fixed bugs

--- disgames/hangman.py
+++ disgames/hangman.py
@@ -15,7 +15,7 @@ class Hangman:
 
     def _show_guesses(self, embed, guesses):
         if guesses:
-            embed.add_field(name="Guesses", value=''.join(f':regional_indicator_{i}:' for i in guesses))
+            embed.add_field(name="Guesses", value=''.join(f':regional_indicator_{i}:' for i in guesses), inline=False)
 
     @commands.command("hangman", aliases=['hm'])
     async def command(self, ctx: commands.Context):
@@ -23,58 +23,50 @@ class Hangman:
         word = list(word)
         guesses = []
         errors = 0
-        revealed_message = '🟦'*len(word)
-        embed = discord.Embed()
-        embed.add_field(name='Hangman', value=self.make_hangman(errors))
-        embed.add_field(name='Word', value=revealed_message)
+        revealed_message = '🟦 '*len(word)
+        embed = discord.Embed(color=discord.Color.blurple())
+        embed.add_field(name='Hangman', value=self.make_hangman(errors), inline=False)
+        embed.add_field(name='Word', value=''.join(revealed_message.split(' ')), inline=False)
         msg = await ctx.send(embed=embed)
 
         while True:
-            embed = discord.Embed()
-            embed.add_field(name='Hangman', value=self.make_hangman(errors))
-            embed.add_field(name='Word', value=''.join(f':regional_indicator_{i}:' for i in word))
-            msg: discord.Message = await ctx.bot.wait_for('message', check = lambda m: m.author == ctx.author and m.channel == ctx.channel)
-            if len(msg.content.lower()) > 1:
-                if msg.content.lower() == ''.join(word):
-                    # embed = discord.Embed()
-                    # embed.add_field(name='Hangman', value=self.make_hangman(errors))
-                    # embed.add_field(name='Word', value=''.join(f':regional_indicator_{i}:' for i in word))
+            embed = discord.Embed(color=discord.Color.blurple())
+            embed.add_field(name='Word', value=''.join(f':regional_indicator_{i}:' for i in word), inline=False)
+            message: discord.Message = await ctx.bot.wait_for('message', check = lambda m: m.author == ctx.author and m.channel == ctx.channel)
+            if len(message.content.lower()) > 1:
+                if message.content.lower() == ''.join(word):
+                    embed.add_field(name='Hangman', value=self.make_hangman(errors), inline=False)
                     self._show_guesses(embed, guesses)
-                    embed.add_field(name="Result:", value="You've won!")
+                    embed.add_field(name="Result:", value="You've won!", inline=False)
                     return await msg.edit(embed = embed)
                 else:
                     await ctx.send("Invalid Syntax: your guess can't be more than 1 letter long or the word itself")
-            elif msg.content.lower().isalpha():
-                guesses.append(msg.content)
-                if msg.content.lower() not in word:
+            elif message.content.lower().isalpha():
+                guesses.append(message.content)
+                if message.content.lower() not in word:
                     errors += 1
-                if errors == 5:
-                        # embed = discord.Embed()
-                        # embed.add_field(name='Hangman', value=self.make_hangman(errors))
-                        # embed.add_field(name='Word', value=''.join(f':regional_indicator_{i}:' for i in word))
-                    if guesses:
-                        embed.add_field(name="Guesses", value=''.join(i if i == '🟦' else f':regional_indicator_{i}:' for i in revealed_message))
-                    embed.add_field(name="Result:", value=f"You lost :pensive:\n word was {''.join(word)}")
-                    return await msg.edit(embed = embed)
-                revealed_message = list(revealed_message)
-                for i in range(len(word)):
-                    if word[i] == msg.content.lower():
-                        revealed_message[i] = f':regional_indicator_{word[i]}:'
-                    elif revealed_message[i] == '🟦':
-                        revealed_message[i] = '🟦'
-                revealed_message = ''.join(revealed_message)
-                if '🟦' not in revealed_message:
-                        # embed = discord.Embed()
-                        # embed.add_field(name='Hangman', value=self.make_hangman(errors))
-                        # embed.add_field(name='Word', value=''.join(f':regional_indicator_{i}:' for i in word))
+                if errors == 6:
+                    embed = discord.Embed(color=discord.Color.blurple())
+                    embed.add_field(name='Hangman', value=self.make_hangman(errors), inline=False)
                     self._show_guesses(embed, guesses)
-                    embed.add_field(name="Result:", value="You've won!")
+                    embed.add_field(name="Result:", value=f"You lost :pensive:\n word was {''.join(word)}", inline=False)
+                    return await msg.edit(embed = embed)
+                revealed_message = revealed_message.split(' ')
+                for i in range(len(word)):
+                    if word[i] == message.content.lower():
+                        revealed_message[i] = f':regional_indicator_{word[i]}:'
+                revealed_message = ' '.join(revealed_message)
+                if '🟦 ' not in revealed_message:
+                    embed.add_field(name='Hangman', value=self.make_hangman(errors), inline=False)
+                    self._show_guesses(embed, guesses)
+                    embed.add_field(name="Result:", value="You've won!", inline=False)
                     return await msg.edit(embed = embed)
                 else:
-                    embed = discord.Embed()
-                    embed.add_field(name='Hangman', value=self.make_hangman(errors))
-                    embed.add_field(name='Word', value=''.join(i if i == '🟦' else f':regional_indicator_{i}:' for i in revealed_message))
+                    embed = discord.Embed(color=discord.Color.blurple())
+                    embed.add_field(name='Hangman', value=self.make_hangman(errors), inline=False)
+                    embed.add_field(name='Word', value=''.join(revealed_message.split(' ')), inline=False)
                     self._show_guesses(embed, guesses)
                     await msg.edit(embed = embed)
             else:
-                await ctx.send(f"Invalid Syntax: {msg.content.lower()} is not a letter")
+                await ctx.send(f"Invalid Syntax: {message.content.lower()} is not a letter")
+
