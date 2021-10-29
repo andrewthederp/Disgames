b39f5c5899e4db1815477a98a63a0e376c143603
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-29 20:02:52 +0530

    Format MadLib

--- disgames/madlib.py
+++ disgames/madlib.py
@@ -3,18 +3,38 @@ from discord.ext import commands
 import aiohttp
 
 class MadLib:
-    @commands.command()
-    async def madlib(self, ctx, min='5', max='25'):
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get(f'http://madlibz.herokuapp.com/api/random?minlength={min}&maxlength={max}') as r:
-                h = await r.json()
-                lst = []
-                for question in h['blanks']:
-                    await ctx.send(f"Please send: {question}")
-                    answer = await ctx.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
-                    lst.append(answer.content)
-                madlib = h['value']
-                string = ''
-                for i in range(len(madlib)-1):
-                    smth += f'{madlib[i]}{lst[i] if len(lst)-1 >= i else ""}'
-                await ctx.send(string)
+
+    def __init__(self) -> None:
+        self._session = None
+
+    @property
+    def url(self):
+        return "http://madlibz.herokuapp.com/api/random"
+
+    @property
+    def session(self):
+        if not self._session:
+            self._session = aiohttp.ClientSession()
+        return self._session
+
+    async def request(self, min: int, max: int):
+        params = {"minlength" : min, "maxlength" : max}
+        response = self.session.get(self.url, params=params)
+        return response.json()
+
+    @commands.command("madlib")
+    async def command(self, ctx, min: int = 5, max: int = 25):
+        json = self.request(min, max)
+        lst = []
+        for question in json['blanks']:
+            await ctx.send(f"Please send: {question}")
+            answer = await ctx.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
+            lst.append(answer.content)
+        madlib = json['value']
+        string = ''.join(
+            f'{madlib[i]}{lst[i] if len(lst)-1 >= i else ""}'
+            for i in range(len(madlib) - 1)
+        )
+        # I dont understand this shit bruh - Marcus
+
+        await ctx.send(string)
