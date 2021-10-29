4e215d98778d79ba1412865ee0a189c13a30a420
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-29 19:19:28 +0530

    Change default to seperator

--- disgames/Board.py
+++ disgames/Board.py
@@ -2,30 +2,30 @@ class Board:
     """A board class that gives a nested which is the board"""
 
     def __init__(
-        self, x, y, default="", coordinates=[], replacment=" ", test=[]
+        self, x, y, seperator="", coordinates=[], replacment=" ", test=[]
     ):
         self.x = x
         self.y = y
-        self._default = default
+        self._seperator = seperator
         self.coors = coordinates
         self.replacment = replacment
         self.test = test
 
     def __str__(self):
         lst = []
-        g = [f"{self._default}{i}" for i in range(self.x + 1)]
+        g = [f"{self.seperator}{i}" for i in range(self.x + 1)]
         lst.append(g)
         for i in range(self.x):
-            lst.append([f"{self._default}{i+1}"])
+            lst.append([f"{self.seperator}{i+1}"])
             for _ in range(self.y):
-                lst[i + 1].append(f"{self._default}")
+                lst[i + 1].append(f"{self.seperator}")
         if self.coors:
             for x, i in enumerate(self.test):
                 for y, j in enumerate(i):
-                    if self.test[x][y] != f"{self._default} ":
+                    if self.test[x][y] != f"{self.seperator} ":
                         lst[x][y] = self.test[x][y]
             for x, y in self.coors:
-                lst[int(x)][int(y)] = f"{self._default}{self.replacment}"
+                lst[int(x)][int(y)] = f"{self.seperator}{self.replacment}"
         return str(lst)
 
     @property
@@ -34,9 +34,9 @@ class Board:
         return self.x, self.y
 
     @property
-    def default(self):
+    def seperator(self):
         """TODO: change the name
 
         Returns the seperator of the board"""
 
-        return self._default
+        return self._seperator
