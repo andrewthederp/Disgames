b315638866ede424a6bf8398e2b87253e373d548
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-10-29 14:18:16 +0200

    Update __init__.py

--- disgames/__init__.py
+++ disgames/__init__.py
@@ -1,8 +1,9 @@
 from .board import Board
 from .ttt import TicTacToe
+from .hangman import Hangman
+from .madlib import MadLib
 
-all_games = [TicTacToe]
-
+all_games = [TicTacToe,Hangman,MadLib]
 
 def register_commands(bot, *, ignore: list = []):
     for cls in all_games:
