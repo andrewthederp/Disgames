6c0d73507c2a4c822aed2b2ec18e3d6ff752a7b1
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-11-03 16:52:36 +0200

    Added chess

--- disgames/mixins/__init__.py
+++ disgames/mixins/__init__.py
@@ -1,9 +1,11 @@
 from .hangman import Hangman
 from .madlib import MadLib
 from .ttt import TicTacToe
+from .chess import Chess
 
 __all__ = (
     "Hangman",
     "MadLib",
-    "TicTacToe"
+    "TicTacToe",
+    "Chess"
 )
