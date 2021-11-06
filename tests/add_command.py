d99ab7c8f3473b3b813876e258d7146b566277f0
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-11-06 11:38:48 +0530

    delete add_command [outdated]

--- tests/add_command.py
+++
@@ -1,6 +0,0 @@
-from disgames import TicTacToe
-from discord.ext import commands
-
-bot = commands.Bot("!", help_command=None)
-bot.add_command(TicTacToe().command)
-assert len(bot.commands) == 1  # only the tictactoe command
