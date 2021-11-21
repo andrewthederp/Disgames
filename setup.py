2dfb3ea68af5cfdff92af012a5eb6906665e6fe7
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-11-21 17:01:47 +0200

    Bumbed version to 1.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ desc = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="disgames",
-    version="1.3.1",
+    version="1.4.1",
     description="A games module that can be used to instantly add games to your discord bot",
     long_description=desc,
     long_description_content_type="text/markdown",
