dfdbdb4c24b4dcfa7556feb6d0adf051d3b36d2e
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-12-10 08:12:14 +0200

    fixed register_commands

--- disgames/__init__.py
+++ disgames/__init__.py
@@ -3,14 +3,13 @@ from .constants import *
 from .errors import PathNotFound
 from discord.ext import commands
 
-def register_commands(
-    bot,
-    *,
-    ignore: list = []
-):
-    games = [
-        game for game in ALL_GAMES if game not in ignore
-    ]
+def register_commands(bot, *, ignore: list = [], stockfish_path=None):
+    games = [game for game in ALL_GAMES if game not in ignore]
     class Games(*games):
-        pass
-    bot.add_cog(Games(bot))
+        def __init__(self, bot):
+            for cls in games:
+                cls.__init__(self, bot)
+    g = Games(bot)
+    if stockfish_path:
+        g.stockfish_path = stockfish_path
+    bot.add_cog(g)
