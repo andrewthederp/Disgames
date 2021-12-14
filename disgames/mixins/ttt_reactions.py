068470f630d3a8183a85d5aa088f7b0c98527dc5
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-12-14 18:14:08 +0200

    Added advanced ai option

--- disgames/mixins/ttt_reactions.py
+++ disgames/mixins/ttt_reactions.py
@@ -2,27 +2,27 @@ import discord
 from discord.ext import commands
 import random
 
-class TicTacToe(commands.Cog):
+class TicTacToeReactions(commands.Cog):
     """
-    Tic Tac Toe command
+    Tic Tac Toe Reactions command
     """
     def __init__(self, bot):
         self.bot = bot
 
-    def format_ttt_board(self, board):
+    def format_ttt_reactions_board(self, board):
         lst = ['  1 2 3']
         for x, row in enumerate(board, start=1):
             lst.append(''.join([str(x)+'|'+'|'.join(row)]))
         return "\n".join(lst)
 
-    def get_empty_ttt(self, board):
+    def get_empty_ttt_reactions(self, board):
         for x, row in enumerate(board):
             for y, column in enumerate(row):
                 if column == ' ':
                     yield x, y
 
-    def minimax_ttt(self, board, depth, isMaximizing):
-        won = self.has_won_ttt(board)
+    def minimax_ttt_reactions(self, board, depth, isMaximizing):
+        won = self.has_won_ttt_reactions(board)
         if won[0] and won[1] == 'x':
             return -1
         elif won[0] and won[1] == 'o':
@@ -32,65 +32,39 @@ class TicTacToe(commands.Cog):
      
         if isMaximizing:
             bestScore = -800
-            for x, y in self.get_empty_ttt(board):
+            for x, y in self.get_empty_ttt_reactions(board):
                 board[x][y] = 'o'
-                score = self.minimax_ttt(board, depth + 1, False)
+                score = self.minimax_ttt_reactions(board, depth + 1, False)
                 board[x][y] = ' '
                 if score > bestScore:
                     bestScore = score
             return bestScore
         else:
             bestScore = 800
-            for x, y in self.get_empty_ttt(board):
+            for x, y in self.get_empty_ttt_reactions(board):
                 board[x][y] = 'x'
-                score = self.minimax_ttt(board, depth + 1, True)
+                score = self.minimax_ttt_reactions(board, depth + 1, True)
                 board[x][y] = ' '
                 if score < bestScore:
                     bestScore = score
             return bestScore
 
-    def make_bot_move_ttt(self, board, difficulty):
+    def make_bot_move_ttt_reactions(self, board, difficulty):
         if difficulty == 1:
-            return random.choice([str(x)+str(y) for x, y in self.get_empty_ttt(board)])
+            return random.choice([str(x)+str(y) for x, y in self.get_empty_ttt_reactions(board)])
         else:
             bestScore = -800
             bestMove = 0
-            for x, y in self.get_empty_ttt(board):
+            for x, y in self.get_empty_ttt_reactions(board):
                 board[x][y] = 'o'
-                score = self.minimax_ttt(board, 0, False)
+                score = self.minimax_ttt_reactions(board, 0, False)
                 board[x][y] = ' '
                 if score > bestScore:
                     bestScore = score
-                    bestMove = str(x), str(y)
+                    bestMove = str(x)+str(y)
             return bestMove
 
-    def make_ttt_move(self, answer, board, turn):
-        if answer not in [
-            "11",
-            "12",
-            "13",
-            "21",
-            "22",
-            "23",
-            "31",
-            "32",
-            "33",
-        ]:
-            return (
-                False,
-                f"Invalid Syntax: {answer} isn't a valid place on the board, Please try again",
-            )
-        else:
-            if board[int(answer[0])-1][int(answer[1])-1] != ' ':
-                return (
-                    False,
-                    f"Invalid Syntax: {answer} has already been chosen, Please try again",
-                )
-            else:
-                board[int(answer[0])-1][int(answer[1])-1] = turn
-                return True, board
-
-    def has_won_ttt(self, board):
+    def has_won_ttt_reactions(self, board):
         BLANK = ' '
         for i in range(3):
 
@@ -111,36 +85,45 @@ class TicTacToe(commands.Cog):
     @commands.command(aliases=["ttt"])
     async def tictactoe(self, ctx: commands.Context, member: discord.Member=None):
         """two players take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner"""
+        conversion = {'↖':'00', '⬆':'01', '↗':'02', '⬅':'10', '⏺️':'11', '➡':'12', '↙':'20', '⬇':'21', '↘':'22', '🏳':'end'}
+        emojis = ['↖', '⬆', '↗', '⬅', '⏺️', '➡', '↙', '⬇', '↘', '🏳']
         if member == None:
             turn = ctx.author
             board = [[' ' for i in range(3)] for i in range(3)]
             await ctx.send("Choose ai difficulty, easy/hard:")
-            msg = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel and m.content.lower() in ['easy', 'hard'])
+            msg = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel and m.content in ['easy', 'hard'])
             difficulty = 1 if msg.content.lower() == 'easy' else 2
             embed = discord.Embed(
                 title="TicTacToe",
-                description=f"How to play: send the coordinates of where you want to place your token, eg: `22`\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_board(board)}\n```",
+                description=f"How to play: React with the emoji pointing to the direction you want to place your token in\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_reactions_board(board)}\n```",
                 color=discord.Color.blurple()
-            ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
+            ).set_footer(text='React with "🏳" to end the game')
             msg = await ctx.send(embed=embed)
+            for emoji in emojis:
+                await msg.add_reaction(emoji)
             while True:
                 if turn == ctx.author:
-                    inp = await ctx.bot.wait_for(
-                        "message",
-                        check=lambda m: m.author == turn and m.channel == ctx.channel,
+                    reaction, user = await ctx.bot.wait_for(
+                        "reaction_add",
+                        check=lambda r, u: u == ctx.author and r.message == msg and str(r) in emojis
                     )
-                    if inp.content.lower() in ["cancel","end",'stop']:
-                        return await ctx.send("Cancelled the game")
-                    outp = self.make_ttt_move(inp.content, board, "x")
-                    if outp[0]:
-                        board = outp[1]
-                    else:
-                        await ctx.send(outp[1], delete_after=5)
-                        continue
+                    coordinates = conversion[reaction.emoji]
+                    if coordinates == 'end':
+                        return await ctx.send("Ended the game")
+                    board[int(coordinates[0])][int(coordinates[1])] = 'x'
+                    try:
+                        await msg.remove_reaction(str(reaction), user)
+                    except discord.Forbidden:
+                        pass
+                    await msg.remove_reaction(str(reaction), self.bot.user)
+                    emojis.remove(str(reaction))
                 else:
-                    move = self.make_bot_move_ttt(board, difficulty)
+                    move = self.make_bot_move_ttt_reactions(board, difficulty)
+                    reaction = {value:key for key, value in conversion.items()}[move]
+                    emojis.remove(reaction)
+                    await msg.remove_reaction(str(reaction), self.bot.user)
                     board[int(move[0])][int(move[1])] = 'o'
-                h = self.has_won_ttt(board)
+                h = self.has_won_ttt_reactions(board)
                 if h[0]:
                     return await ctx.send(
                         embed=discord.Embed(
@@ -150,12 +133,12 @@ class TicTacToe(commands.Cog):
                                 "winner: `"
                                 f"{turn.display_name}`"
                                 "\n"
-                                f"```\n{self.format_ttt_board(board)}```"
+                                f"```\n{self.format_ttt_reactions_board(board)}```"
                                 "\n"
                             ),
                         )
                     )
-                elif h[0] == False:
+                if h[0] == False:
                     return await ctx.send(
                         embed=discord.Embed(
                             color = discord.Color.blurple(),
@@ -164,7 +147,7 @@ class TicTacToe(commands.Cog):
                                 "winner: `"
                                 "Tie`"
                                 "\n"
-                                f"```\n{self.format_ttt_board(board)}```"
+                                f"```\n{self.format_ttt_reactions_board(board)}```"
                                 "\n"
                             ),
                         )
@@ -173,9 +156,9 @@ class TicTacToe(commands.Cog):
                 await msg.edit(
                     embed=discord.Embed(
                         title="TicTacToe",
-                        description=f"How to play: send the coordinates of where you want to place your token, eg: `22`\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_board(board)}\n```",
+                        description=f"How to play: React with the emoji pointing to the direction you want to place your token in\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_reactions_board(board)}\n```",
                         color=discord.Color.blurple()
-                    ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
+                    ).set_footer(text='React with "🏳" to end the game')
                 )
         elif member.bot or member == ctx.author:
             return await ctx.send(
@@ -186,26 +169,28 @@ class TicTacToe(commands.Cog):
             board = [[' ' for i in range(3)] for i in range(3)]
             embed = discord.Embed(
                 title="TicTacToe",
-                description=f"How to play: send the coordinates of where you want to place your token, eg: `22`\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_board(board)}\n```",
+                description=f"How to play: React with the emoji pointing to the direction you want to place your token in\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_reactions_board(board)}\n```",
                 color=discord.Color.blurple()
-            ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
+            ).set_footer(text='React with "🏳" to end the game')
             msg = await ctx.send(embed=embed)
+            for emoji in emojis:
+                await msg.add_reaction(emoji)
             while True:
-                inp = await ctx.bot.wait_for(
-                    "message",
-                    check=lambda m: m.author == turn and m.channel == ctx.channel,
+                reaction, user = await ctx.bot.wait_for(
+                    "reaction_add",
+                    check=lambda r, u: u == turn and r.message == msg and str(r) in emojis
                 )
-                if inp.content in ["cancel",'end','stop']:
-                    return await ctx.send("Cancelled the game")
-                outp = self.make_ttt_move(
-                    inp.content, board, "x" if turn == ctx.author else "o"
-                )
-                if outp[0]:
-                    board = outp[1]
-                else:
-                    await ctx.send(outp[1])
-                    continue
-                h = self.has_won_ttt(board)
+                coordinates = conversion[reaction.emoji]
+                if coordinates == 'end':
+                    return await ctx.send("Ended the game")
+                board[int(coordinates[0])][int(coordinates[1])] = "x" if turn == ctx.author else 'o'
+                try:
+                    await msg.remove_reaction(str(reaction), user)
+                except discord.Forbidden:
+                    pass
+                await msg.remove_reaction(str(reaction), self.bot.user)
+                emojis.remove(str(reaction))
+                h = self.has_won_ttt_reactions(board)
                 if h[0]:
                     return await ctx.send(
                         embed=discord.Embed(
@@ -215,10 +200,10 @@ class TicTacToe(commands.Cog):
                                 "winner: `"
                                 f"{turn.display_name}`"
                                 "\n"
-                                f"```\n{self.format_ttt_board(board)}```"
+                                f"```\n{self.format_ttt_reactions_board(board)}```"
                                 "\n"
                             ),
-                        ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
+                        ).set_footer(text='React with "🏳" to end the game')
                     )
                 elif h[0] == False:
                     return await ctx.send(
@@ -227,18 +212,18 @@ class TicTacToe(commands.Cog):
                             color=discord.Color.blurple(),
                             description=(
                                 "winner: `"
-                                f"Tie`"
+                                "Tie`"
                                 "\n"
-                                f"```\n{self.format_ttt_board(board)}```"
+                                f"```\n{self.format_ttt_reactions_board(board)}```"
                                 "\n"
                             ),
-                        ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
+                        ).set_footer(text='React with "🏳" to end the game')
                     )
                 turn = member if turn == ctx.author else ctx.author
                 await msg.edit(
                     embed=discord.Embed(
                         title="TicTacToe",
-                        description=f"How to play: send the coordinates of where you want to place your token, eg: `22`\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_board(board)}\n```",
+                        description=f"How to play: React with the emoji pointing to the direction you want to place your token in\n\nturn: `{turn.display_name}`\n```\n{self.format_ttt_reactions_board(board)}\n```",
                         color=discord.Color.blurple()
-                    ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
+                    ).set_footer(text='React with "🏳" to end the game')
                 )
