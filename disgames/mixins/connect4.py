2d6d00f8a76305c0158aa72c7ae37eeab4869326
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2021-12-29 20:38:10 +0200

    Added ai option

--- disgames/mixins/connect4.py
+++ disgames/mixins/connect4.py
@@ -17,8 +17,10 @@ class Connect4(commands.Cog):
         toDisplay += "1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣"
         return toDisplay
 
-    def has_won_connect4(self, height, width, board):
+    def has_won_connect4(self, board, is_bot=False):
         """Checks if game is over"""
+        height = 6
+        width = 7
         for x in range(height):
             for y in range(width - 3):
                 if (
@@ -27,17 +29,18 @@ class Connect4(commands.Cog):
                     and board[x][y] == board[x][y + 3]
                     and board[x][y] != " "
                 ):
-                    if board[x][y] == "b":
-                        board[x][y] = "B"
-                        board[x][y + 1] = "B"
-                        board[x][y + 2] = "B"
-                        board[x][y + 3] = "B"
-                    elif board[x][y] == "r":
-                        board[x][y] = "R"
-                        board[x][y + 1] = "R"
-                        board[x][y + 2] = "R"
-                        board[x][y + 3] = "R"
-                    return True, board, "in a horizontal row"
+                    if not is_bot:
+                        if board[x][y] == "b":
+                            board[x][y] = "B"
+                            board[x][y + 1] = "B"
+                            board[x][y + 2] = "B"
+                            board[x][y + 3] = "B"
+                        elif board[x][y] == "r":
+                            board[x][y] = "R"
+                            board[x][y + 1] = "R"
+                            board[x][y + 2] = "R"
+                            board[x][y + 3] = "R"
+                    return True, board, "in a horizontal row", board[x][y].lower()
         for x in range(height - 3):
             for y in range(width):
                 if (
@@ -46,17 +49,18 @@ class Connect4(commands.Cog):
                     and board[x][y] == board[x + 3][y]
                     and board[x][y] != " "
                 ):
-                    if board[x][y] == "b":
-                        board[x][y] = "B"
-                        board[x + 1][y] = "B"
-                        board[x + 2][y] = "B"
-                        board[x + 3][y] = "B"
-                    elif board[x][y] == "r":
-                        board[x][y] = "R"
-                        board[x + 1][y] = "R"
-                        board[x + 2][y] = "R"
-                        board[x + 3][y] = "R"
-                    return True, board, "in a vertical row"
+                    if not is_bot:
+                        if board[x][y] == "b":
+                            board[x][y] = "B"
+                            board[x + 1][y] = "B"
+                            board[x + 2][y] = "B"
+                            board[x + 3][y] = "B"
+                        elif board[x][y] == "r":
+                            board[x][y] = "R"
+                            board[x + 1][y] = "R"
+                            board[x + 2][y] = "R"
+                            board[x + 3][y] = "R"
+                    return True, board, "in a vertical row", board[x][y].lower()
         for x in range(height - 3):
             for y in range(width - 3):
                 if (
@@ -65,17 +69,18 @@ class Connect4(commands.Cog):
                     and board[x][y] == board[x + 3][y + 3]
                     and board[x][y] != " "
                 ):
-                    if board[x][y] == "b":
-                        board[x][y] = "B"
-                        board[x + 1][y + 1] = "B"
-                        board[x + 2][y + 2] = "B"
-                        board[x + 3][y + 3] = "B"
-                    elif board[x][y] == "r":
-                        board[x][y] = "R"
-                        board[x + 1][y + 1] = "R"
-                        board[x + 2][y + 2] = "R"
-                        board[x + 3][y + 3] = "R"
-                    return True, board, "on a \ diagonal"
+                    if not is_bot:
+                        if board[x][y] == "b":
+                            board[x][y] = "B"
+                            board[x + 1][y + 1] = "B"
+                            board[x + 2][y + 2] = "B"
+                            board[x + 3][y + 3] = "B"
+                        elif board[x][y] == "r":
+                            board[x][y] = "R"
+                            board[x + 1][y + 1] = "R"
+                            board[x + 2][y + 2] = "R"
+                            board[x + 3][y + 3] = "R"
+                    return True, board, "on a \ diagonal", board[x][y].lower()
         for x in range(height - 3):
             for y in range(3, width):
                 if (
@@ -84,17 +89,18 @@ class Connect4(commands.Cog):
                     and board[x][y] == board[x + 3][y - 3]
                     and board[x][y] != " "
                 ):
-                    if board[x][y] == "b":
-                        board[x][y] = "B"
-                        board[x + 1][y - 1] = "B"
-                        board[x + 2][y - 2] = "B"
-                        board[x + 3][y - 3] = "B"
-                    elif board[x][y] == "r":
-                        board[x][y] = "R"
-                        board[x + 1][y - 1] = "R"
-                        board[x + 2][y - 2] = "R"
-                        board[x + 3][y - 3] = "R"
-                    return True, board, "in a / diagonal"
+                    if not is_bot:
+                        if board[x][y] == "b":
+                            board[x][y] = "B"
+                            board[x + 1][y - 1] = "B"
+                            board[x + 2][y - 2] = "B"
+                            board[x + 3][y - 3] = "B"
+                        elif board[x][y] == "r":
+                            board[x][y] = "R"
+                            board[x + 1][y - 1] = "R"
+                            board[x + 2][y - 2] = "R"
+                            board[x + 3][y - 3] = "R"
+                    return True, board, "in a / diagonal", board[x][y].lower()
         num = 0
         for row in board:
             for column in row:
@@ -104,22 +110,91 @@ class Connect4(commands.Cog):
             return False, board, "Tie"
         return None, None, None
 
+    def get_empty_connect4(self, board):
+        """Yields every empty index on the board"""
+        for x, _ in enumerate(board):
+            y = 0
+            while y != 6:
+                if board[5 - y][x] == " ":
+                    yield 5-y, x
+                    break
+                else:
+                    y += 1
+
+    def minimax_connect4(self, board, depth, isMaximizing):
+        """The minimax algorithm for ttt"""
+        won = self.has_won_connect4(board, True)
+        if depth == 6:
+            return 1
+        elif won[0] and won[3] == "r":
+            return -1
+        elif won[0] and won[3] == "b":
+            return 1
+        elif won[0] == False:
+            return 0
+
+        if isMaximizing:
+            bestScore = -800
+            for x, y in self.get_empty_connect4(board):
+                board[x][y] = "b"
+                score = self.minimax_connect4(board, depth + 1, False)
+                board[x][y] = " "
+                if score > bestScore:
+                    bestScore = score
+            return bestScore
+        else:
+            bestScore = 800
+            for x, y in self.get_empty_connect4(board):
+                board[x][y] = "r"
+                score = self.minimax_connect4(board, depth + 1, True)
+                board[x][y] = " "
+                if score < bestScore:
+                    bestScore = score
+            return bestScore
+
+    def make_bot_move_connect4(self, board, difficulty):
+        """Returns the best move the bot can take"""
+        if difficulty == 1:
+            x, y = random.choice([move for move in self.get_empty_connect4(board)])
+            board[x][y] = 'b'
+            return board
+        else:
+            bestScore = -800
+            bestMove = 0
+            for x, y in self.get_empty_connect4(board):
+                board[x][y] = "o"
+                score = self.minimax_connect4(board, 0, False)
+                board[x][y] = " "
+                if score > bestScore:
+                    bestScore = score
+                    bestMove = x, y
+            board[bestMove[0]][bestMove[1]] = 'b'
+            return board
+
     @commands.command()
     async def connect4(self, ctx, member: discord.Member = None):
         """a two-player connection board game, in which the players take turns dropping colored discs into a seven-column, six-row vertically suspended grid."""
         if not member:
             board = [[" " for _ in range(7)] for i in range(6)]
+            turn = ctx.author
+            await ctx.send("Choose ai difficulty, easy/hard:")
+            msg = await self.bot.wait_for(
+                "message",
+                check=lambda m: m.author == ctx.author
+                and m.channel == ctx.channel
+                and m.content.lower() in ["easy", "hard"],
+            )
+            difficulty = 1 if msg.content.lower() == "easy" else 2
             e = discord.Embed(
                 title="Connect4",
-                description=f"How to play: type a number 1-7 to drop a token inside that column\n\n{self.format_connect4_board(board)}",
+                description=f"How to play: type a number 1-7 to drop a token inside that column\nturn: `{turn.display_name}`\n\n{self.format_connect4_board(board)}",
                 color=discord.Color.blurple(),
             ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
             msg = await ctx.send(embed=e)
-            turn = ctx.author
             while True:
                 e = discord.Embed(
                     title="Connect4",
-                    description=f"How to play: type a number 1-7 to drop a token inside that column\n\n{self.format_connect4_board(board)}",
+                    description=f"How to play: type a number 1-7 to drop a token inside that column\nturn: `{turn.display_name}`\n\n{self.format_connect4_board(board)}",
                     color=discord.Color.blurple(),
                 ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
                 await msg.edit(embed=e)
@@ -155,22 +230,9 @@ class Connect4(commands.Cog):
                         else:
                             y += 1
                 else:
-                    x = random.randint(0, 6)
-                    h = True
-                    while h:
-                        y = 0
-                        while y <= 6:
-                            if y == 6:
-                                x = random.randint(0, 6)
-                                break
-                            if board[5 - y][x] == " ":
-                                board[5 - y][x] = "r" if turn == ctx.author else "b"
-                                h = False
-                                break
-                            else:
-                                y += 1
+                    board = self.make_bot_move_connect4(board, difficulty)
 
-                won = self.has_won_connect4(6, 7, board)
+                won = self.has_won_connect4(board)
                 if won[0]:
                     await ctx.send(f"{turn.mention} connected 4 {won[2]}")
                     e = discord.Embed(
@@ -196,7 +258,7 @@ class Connect4(commands.Cog):
             board = [[" " for _ in range(7)] for i in range(6)]
             e = discord.Embed(
                 title="Connect4",
-                description=f"How to play: type a number 1-7 to drop a token inside that column\n\n{self.format_connect4_board(board)}",
+                description=f"How to play: type a number 1-7 to drop a token inside that column\nturn: `{turn.display_name}`\n\n{self.format_connect4_board(board)}",
                 color=discord.Color.blurple(),
             ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
             msg = await ctx.send(embed=e)
@@ -204,7 +266,7 @@ class Connect4(commands.Cog):
             while True:
                 e = discord.Embed(
                     title="Connect4",
-                    description=f"How to play: type a number 1-7 to drop a token inside that column\n\n{self.format_connect4_board(board)}",
+                    description=f"How to play: type a number 1-7 to drop a token inside that column\nturn: `{turn.display_name}`\n\n{self.format_connect4_board(board)}",
                     color=discord.Color.blurple(),
                 ).set_footer(text='Send "end"/"stop"/"cancel" to stop the game')
                 await msg.edit(embed=e)
@@ -238,7 +300,7 @@ class Connect4(commands.Cog):
                         break
                     else:
                         y += 1
-                won = self.has_won_connect4(6, 7, board)
+                won = self.has_won_connect4(board)
                 if won[0]:
                     await ctx.send(f"{turn.mention} connected 4 {won[2]}")
                     e = discord.Embed(
