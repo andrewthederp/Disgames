2a05fa13de5f8bc20c225e64f3eac29d184b2392
Jerrydotpy <96548672+Jerrydotpy@users.noreply.github.com>
2022-01-11 17:19:48 -0500

    Fix mistake

--- disgames/__main__.py
+++ disgames/__main__.py
@@ -7,7 +7,7 @@ def version() -> None:
     entries = []
 
     entries.append('- Python v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}'.format(sys.version_info))
-    version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=1
+    version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=1)
     entries.append('- Disgames v{0.major}.{0.minor}.{0.micro}'.format(version_info))
     entries.append(f'- aiohttp v{aiohttp.__version__}') 
     entries.append(f'- Chess v{chess.__version__}')
