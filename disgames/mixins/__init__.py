cb13e58c381e4d6bf98c18131a0cfeae4d38cb07
pusheen3 <theofficialjerrypy@gmail.com>
2022-01-10 19:17:19 -0500

    Fix typo

--- disgames/mixins/__init__.py
+++ disgames/mixins/__init__.py
@@ -1,13 +1,13 @@
+from .checkers import Checkers
+from .chess import Chess
 from .hangman import Hangman
 from .madlib import MadLib
 from .ttt import TicTacToe
-from .chess import Chess
 from .minesweeper import Minesweeper
 from .soko import Sokoban
 from .rps import RPS
 from .two048 import _2048
 from .connect4 import Connect4
-from .checkers import Checkers
 from .sudoko import Sudoko
 from .ttt_reactions import TicTacToeReactions
 from .battleship import Battleships
@@ -28,6 +28,6 @@ __all__ = (
     "Sudoko",
     "TicTacToeReactions",
     "Battleships",
-    "Snake,
+    "Snake",
     "SNL",
 )
