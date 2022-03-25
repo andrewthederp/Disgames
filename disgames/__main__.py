6f626a4616bfe89aca921e1806e0074f216a11da
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-03-25 03:56:30 +0200

    bumped version to 2.3.1

--- disgames/__main__.py
+++ disgames/__main__.py
@@ -19,7 +19,7 @@ def version() -> None:
     ]
 
 
-    version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=1)
+    version_info: VersionInfo = VersionInfo(major=2, minor=3, micro=1)
     entries.append('- Disgames v{0.major}.{0.minor}.{0.micro}'.format(version_info))
     entries.append(f'- aiohttp v{aiohttp.__version__}')
     entries.append(f'- Chess v{chess.__version__}')
