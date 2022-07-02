2143f48d19e196febf7482612f571f39387c19cd
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-07-02 06:33:24 +0200

    Delete tests directory

--- tests/ignore.py
+++
@@ -1,7 +0,0 @@
-from disgames import Chess, register_commands
-from discord.ext import commands
-
-bot = commands.Bot("!", help_command=None)
-register_commands(bot, ignore=[Chess])
-
-assert len(bot.commands) == 10  # madlib, ttt, hangman, etc
--- tests/register_commands.py
+++
@@ -1,6 +0,0 @@
-from disgames import register_commands
-from discord.ext import commands
-
-bot = commands.Bot("!", help_command=None, case_insensitive=True)
-register_commands(bot)
-assert len(bot.commands) == 11  # chess, madlib, ttt, hangman, etc
