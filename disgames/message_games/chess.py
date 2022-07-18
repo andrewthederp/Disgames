ccd64c1fba3d2d939de8b22116c48862e26cd086
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-07-18 05:09:16 +0200

    Fixing more stupid mistakes

--- disgames/message_games/chess.py
+++ disgames/message_games/chess.py
@@ -53,7 +53,7 @@ class Chess:
 		self.game.setup(self.board)
 		self.node = self.game
 
-	async def has_won(self):
+	def has_won(self):
 		value = None
 		result = self.board.result()
 
@@ -105,7 +105,6 @@ class Chess:
 	def make_embed(self):
 		won = self.has_won()
 		if won:
-			print(won)
 			self.game.headers["Termination"] = won
 		embed = discord.Embed(title='Chess', description=won or f"Turn: {self.turns[self.turn].mention} ({self.colors[self.turn]})", color=ongoing_game_color)
 		embed.add_field(name=f"Legal moves", value=", ".join([f"`{str(i)}`" for i in self.board.legal_moves]) or 'No legal moves', inline=False)
